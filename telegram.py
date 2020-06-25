4c84086a51b22f6fd3fb57c4d1e7f7d61288673b
Riley Inman <rileyminman@gmail.com>
2020-06-25 11:06:09 -0400

    telegram: Bring in line with Discord plugin to hopefully fix problems

--- telegram.py
+++ telegram.py
@@ -1,34 +1,62 @@
 import logging
 from pwnagotchi.voice import Voice
 import pwnagotchi.plugins as plugins
+import os
 
 
 class Telegram(plugins.Plugin):
     __author__ = 'djerfy@gmail.com'
     __version__ = '1.0.0'
     __license__ = 'GPL3'
-    __description__ = 'Periodically sent messages to Telegram about the recent activity of pwnagotchi'
+    __description__ = 'Post recent activity to Telegram. Requires python-telegram-bot.'
+
+    def __init__(self):
+        self.ready = False
 
     def on_loaded(self):
+        try:
+            import telegram
+        except ImportError as e:
+            logging.error("[telegram] Couldn't import python library.")
+            logging.debug(e);
+            return
+
+        if 'bot_token' not in self.options or not self.options['bot_token']:
+            logging.error("[telegram] Token not set, cannot send to Telegram.")
+            return
+
+        if 'chat_id' not in self.options or not self.options['chat_id']:
+            logging.error("[telegram] Chat ID not set, cannot send to Telegram.")
+            return
+
+        if self.options['send_picture'] is False and self.options['send_message'] is False:
+            logging.warning("[telegram] Pictures and messages are both disabled.")
+
+        self.ready = True
         logging.info("[telegram] Plugin loaded.")
 
     # called when there's available internet
     def on_internet_available(self, agent):
+        if not self.ready:
+            return
+
         config = agent.config()
         display = agent.view()
         last_session = agent.last_session
 
         if last_session.is_new() and last_session.handshakes > 0:
-
             try:
                 import telegram
-            except ImportError:
+            except ImportError as e:
                 logging.error("[telegram] Couldn't import python library.")
+                logging.debug(e);
                 return
 
             logging.info("[telegram] Detected new activity and internet, time to send a message!")
 
-            picture = '/root/pwnagotchi.png'
+            picture =
+                '/var/tmp/pwnagotchi/pwnagotchi.png' if os.path.exists("/var/tmp/pwnagotchi/pwnagotchi.png")
+                else '/root/pwnagotchi.png'
             display.on_manual_mode(last_session)
             display.image().save(picture, 'png')
             display.update(force=True)
@@ -37,17 +65,21 @@ class Telegram(plugins.Plugin):
                 logging.info("[telegram] Connecting to Telegram...")
 
                 message = Voice(lang=config['main']['lang']).on_last_session_tweet(last_session)
-
                 bot = telegram.Bot(self.options['bot_token'])
+
                 if self.options['send_picture'] is True:
+                    logging.info("[telegram] Sending picture...")
                     bot.sendPhoto(chat_id=self.options['chat_id'], photo=open(picture, 'rb'))
                     logging.info("[telegram] Picture sent.")
+
                 if self.options['send_message'] is True:
+                    logging.info("[telegram] Sending message...")
                     bot.sendMessage(chat_id=self.options['chat_id'], text=message, disable_web_page_preview=True)
-                    logging.info("[telegram] Message sent: %s" % message)
+                    logging.info(f"[telegram] Message sent: {message}")
 
                 last_session.save_session_id()
                 display.set('status', 'Telegram notification sent!')
                 display.update(force=True)
             except Exception as e:
-                logging.exception(f"[telegram] Error while sending on Telegram: {e}.")
+                logging.exception("[telegram] Error while sending message.")
+                logging.debug(e)
